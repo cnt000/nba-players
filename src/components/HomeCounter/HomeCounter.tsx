e3d8012ddb67dc7a2c4087d08978bb4c82e383bb
Edoardo Gargano <edoardo.gargano@monrif.net>
2021-04-01 17:53:33 +0200

    lint fix

--- src/components/HomeCounter/HomeCounter.tsx
+++ src/components/HomeCounter/HomeCounter.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import {
-  StyledContainer,
-  StyledHeader,
-  StyledLogo,
-} from './HomeCounter.styles'
+import { StyledContainer, StyledHeader, StyledLogo } from './HomeCounter.styles'
 import logo from './logo.svg'
 import Button from '@material-ui/core/Button'
 import { Counter } from '../../state/features/counter/Counter'
@@ -18,7 +14,11 @@ const HomeCounter: React.FC = () => {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <div>
-          <Button variant="contained" color="primary" onClick={() => alert('ok')}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => alert('ok')}
+          >
             Hello World
           </Button>
         </div>
