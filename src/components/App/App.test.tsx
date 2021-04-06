6a63bb89beb15e7fe82fba59725c3beabc7305aa
Edoardo Gargano <edoardo.gargano@monrif.net>
2021-04-06 18:39:56 +0200

    lint

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
-import { store } from '../../state/store'
 import App from './App'
 import { theme } from '../../styles/theme'
 
 test('renders title of application', () => {
   const { getByText } = render(
     <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </ThemeProvider>
   )
 
